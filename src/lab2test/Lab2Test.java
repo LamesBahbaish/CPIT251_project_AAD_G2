53dd0fc76a3e1f614825143f97531431d8675eeb
Lames <lbahbaish@stu.kau.edu.sa>
2024-02-12 19:23:13 +0300

    Test statemnt

--- src/lab2test/Lab2Test.java
+++ src/lab2test/Lab2Test.java
@@ -15,7 +15,7 @@ public class Lab2Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    System.out.print("HI I am Lames");
+    System.out.print("I am Lames");
     }
     
 }

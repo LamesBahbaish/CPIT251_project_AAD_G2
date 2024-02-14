251636b5bfd1e9440b256c88f2109e5dcb4e7873
Joud_Alharbi <Joudalharbbi7@gmail.com>
2024-02-14 14:38:11 +0300

    Joud is Commit!

--- src/lab2test/Lab2Test.java
+++ src/lab2test/Lab2Test.java
@@ -17,6 +17,7 @@ public class Lab2Test {
     public static void main(String[] args) {
     System.out.print("I am Lames");
     System.out.println("I am Leena");
+    System.out.println("I am Joud");
     }
     
 }
